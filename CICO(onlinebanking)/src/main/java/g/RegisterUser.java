b7725d18fde3b19703da092de6141980b154af7b
Rama Krishna Bhairava <154914032+RamaKrishna1310@users.noreply.github.com>
2024-02-17 20:18:35 +0530

    RegisterUser.java

--- CICO(onlinebanking)/src/main/java/g/RegisterUser.java
+++ CICO(onlinebanking)/src/main/java/g/RegisterUser.java
@@ -4,16 +4,14 @@ import java.sql.*;
 import g.GetCon;
 public class RegisterUser {
 static int status=0;
-//int accountno=1;
-public static int register(String username,String password,String repassword,double amount,String adderess,String phone){
-	//public static int register(String email,String password,String gender,String country,String name){
 
+public static int register(String username,String password,String repassword,double amount,String adderess,String phone){
+	
 	Connection con=GetCon.getCon();
 	PreparedStatement ps;
 	try {
 		ps = con.prepareStatement("Insert into NEWACCOUNT(USERNAME, PASSWORD, REPASSWORD, AMOUNT, ADDERESS, PHONE) values(?,?,?,?,?,?)");
-//		int	nextvalue1=GetCon.getPrimaryKey();
-		//ps.setInt(1,null);
+
 		ps.setString(1,username);
 		ps.setString(2,password);
 		ps.setString(3,repassword);
